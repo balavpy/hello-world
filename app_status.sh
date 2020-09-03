9767c2f29ddb9f422683d7f2f8da8ad4474a10f3
Balakrishnan R <59216990+balavpy@users.noreply.github.com>
2020-09-03 06:17:58 +0530

    Update app_status.sh

--- app_status.sh
+++ app_status.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 http_url=`kubectl get svc  webapp  | cut -d' ' -f10 | tail -1`
 httpstatus=`curl -s -o /dev/null -w "%{http_code}" ${http_url}:8080/webapp/`
-if [ $httpstatus == '200' ]; then;
+if [ $httpstatus == "200" ]; then
   echo "http status is  '${httpstatus}'- Application is Up & Running"
 else
   echo "http status is  '${httpstatus}' "
